f5278520a158fcca9adae1fd0285568be3e76db4
iddrisu sumaila <iddrisu.sumaila@amalitech.org>
2020-12-29 08:48:50 +0000

    i learnt a new tech known as react and i am very to have learn it

---
+++ src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Cards from '../Cards/Cards'
+import Footer from '../Footer/Footer'
+import HeroSection from '../HeroSection/HeroSection'
+
+
+
+const Home = () => {
+    return (
+        <>
+            <HeroSection/>
+            <Cards/>
+            <Footer/>
+
+        </>
+    )
+}
+
+export default Home
